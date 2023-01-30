aa772e20a1c57147951182a5575fa4eb2a8d9789
MinkyoLee <redfowl@naver.com>
2023-01-30 19:15:14 +0900

    Initial Commit

---
+++ SplitDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SplitDemo
+//
+//  Created by 이민교 on 2023/01/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplitDemo/SplitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitDemoApp.swift
+//  SplitDemo
+//
+//  Created by 이민교 on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct SplitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
